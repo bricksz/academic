ca83224c37fb3234c373dd7f846b2556b44ae989
bricksz <36253313+bricksz@users.noreply.github.com>
2018-03-28 00:22:27 -0400

    Add files via upload

---
+++ cs127/assignment 41.py
@@ -0,0 +1,13 @@
+#terry huang
+#3/27/2018
+#assigntment 40
+
+import folium
+#folium for mapmaking
+
+output = 'nycMap.html'
+#mapworld = folium.Map(location=[0,0],zoom_start=3)  #centered (0,0), zoomed out size 3
+mapnyc = folium.Map(location=[40.75, -74.125], zoom_start=10)
+folium.Marker(location=[40.768731,-73.964915], popup = 'Hunter College').add_to(mapnyc) #this is self-explanatory
+
+mapnyc.save(output)
