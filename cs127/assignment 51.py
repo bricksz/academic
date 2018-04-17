499a416f6290da3f45175d99d559dda041c25153
bricksz <36253313+bricksz@users.noreply.github.com>
2018-04-17 16:33:54 -0400

    Add files via upload

---
+++ cs127/assignment 51.py
@@ -0,0 +1,10 @@
+#terry huang
+#4/16/2018
+#assignment 51
+
+x = input('Enter a non-empty string: ')
+while x == '':
+    x = input('Enter a non-empty string: ')
+    print('That was empty. Try again.')
+else:
+    print(x)
