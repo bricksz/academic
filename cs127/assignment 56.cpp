ddc9e51b95f89626c84d356bbed4890ba7e740b3
bricksz <36253313+bricksz@users.noreply.github.com>
2018-04-19 16:57:15 -0400

    Add files via upload

---
+++ cs127/assignment 56.cpp
@@ -0,0 +1,25 @@
+// terry huang
+// 4/18/2018
+// assignment 56
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "Enter a number: ";
+	cin >> num; 
+	
+	for (int i = num; i > 0; i--)
+	{
+		for (int j = i; j > 0; j--)
+		{
+		cout << "*" ;
+		}
+		
+	cout << endl;
+	}
+	
+}
+		
